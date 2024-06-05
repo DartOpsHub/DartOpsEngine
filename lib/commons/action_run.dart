235ab5816adce9706dd100d6acb62558c8b7f831
josercc <josercc@163.com>
2024-06-05 10:36:17 +0800

    - 🟢[Add] 新增从Env和Map获取值，不存在就抛出异常的方法

--- lib/commons/action_run.dart
+++ lib/commons/action_run.dart
@@ -3,3 +3,23 @@ import 'package:dart_ops_engine/commons/env.dart';
 abstract class ActionRun {
   Future<Map> run(Env env, Map request);
 }
+
+extension EnvGet on Env {
+  String read(String name) {
+    final value = this[name];
+    if (value == null) {
+      throw Exception('环境变量:$name 不存在!');
+    }
+    return value;
+  }
+}
+
+extension MapGet on Map {
+  T read<T>(String name) {
+    final value = this[name];
+    if (value == null) {
+      throw Exception('没有设置$name 参数!');
+    }
+    return value as T;
+  }
+}
